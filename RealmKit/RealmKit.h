45c79e4df861ea580ddba9d6eec4985437d32803
Michael Loistl <michael@aplo.co>
2015-10-12 16:29:30 +0100

    Initial Commit

---
+++ RealmKit/RealmKit.h
@@ -0,0 +1,19 @@
+//
+//  RealmKit.h
+//  RealmKit
+//
+//  Created by Michael Loistl on 12/10/2015.
+//  Copyright © 2015 Aplo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmKit.
+FOUNDATION_EXPORT double RealmKitVersionNumber;
+
+//! Project version string for RealmKit.
+FOUNDATION_EXPORT const unsigned char RealmKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmKit/PublicHeader.h>
+
+
