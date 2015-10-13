42f67324031060deab1c0b0af505f1018d5a352d
Michael Loistl <michael@aplo.co>
2015-10-13 07:37:14 +0100

    Changing accessibility for RealmValueTransformer methods

--- RealmKit/RealmJSONSerializer.swift
+++ RealmKit/RealmJSONSerializer.swift
@@ -189,18 +189,18 @@ public class RealmValueTransformer: NSValueTransformer {
     
     // Returns a transformer which transforms values using the given closure.
     // Reverse transformations will not be allowed.
-    class func transformerWithClosure(closure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
+    public class func transformerWithClosure(closure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
         return RealmValueTransformer(forwardClosure: closure, reverseClosure: nil)
     }
     
     // Returns a transformer which transforms values using the given closure, for
     // forward or reverse transformations.
-    class func reversibleTransformerWithClosure(closure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
+    public class func reversibleTransformerWithClosure(closure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
         return reversibleTransformerWithForwardBlock(closure, reverseClosure: closure)
     }
     
     // Returns a transformer which transforms values using the given closures.
-    class func reversibleTransformerWithForwardBlock(forwardClosure: (value: AnyObject?) -> AnyObject?, reverseClosure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
+    public class func reversibleTransformerWithForwardBlock(forwardClosure: (value: AnyObject?) -> AnyObject?, reverseClosure: (value: AnyObject?) -> AnyObject?) -> NSValueTransformer! {
         return RealmReversibleValueTransformer(forwardClosure: forwardClosure, reverseClosure: reverseClosure)
     }
     
